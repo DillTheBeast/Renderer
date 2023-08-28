b624ceb4701a9f0f106e035dc0b7ea55c016cb03
DillTheBeast <dillonmaltese@gmail.com>
2023-08-27 22:39:56 -0400

    asd

--- Star.py
+++ Star.py
@@ -1,7 +1,7 @@
 from random import randint
 
-def Star:
-    def __init__(x,y,z):
-        x = randint(0, width)
-        y = randint(0, height)
-        z = randint(0, width)
+class Star:
+    def __init__(self):
+        self.x = randint(0, 400)
+        self.y = randint(0, 400)
+        self.z = randint(0, 400)
--- Starfield.py
+++ Starfield.py
@@ -1,4 +1,28 @@
-def setup:
-    size(400, 400)
+from random import randint
+from star import Star
+import turtle
 
-def draw:
+num_stars = 100
+stars = [Star() for _ in range(num_stars)]
+
+
+# for star in stars:
+#     print(f"Star coordinates: x={star.x}, y={star.y}, z={star.z}")
+
+def setup():
+    screen = turtle.Screen()
+    screen.setup(400, 400)
+    screen.bgcolor("black")
+
+    for i in range(len(stars)):
+        stars[i].add(Star(), i)
+
+    circle = turtle.Turtle()
+    circle.shape("circle")
+    circle.color("dark red")
+
+
+    screen.mainloop()
+
+
+setup()
