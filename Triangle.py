ec3d5598746eda7c89aa3722e5d410d7efbca551
DillTheBeast <dillonmaltese@gmail.com>
2023-10-25 10:53:40 -0400

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -5,8 +5,8 @@ img = Image.new('RGB', (800, 600), color='white')
 
 # Use the ImageDraw module to fill the image
 draw = ImageDraw.Draw(img)
-for x in range(-200, 200):
-    for y in range(-200, 200):
-        draw.point((x + 400, y + 300), fill='red')  # Adjusting for center
+for x in range(100, 300):
+    for y in range(100, 300):
+        draw.point((x,y), fill='red')  # Adjusting for center
 
 img.show()
