e05fa0fac2d3f34a133874147606a1dd8d4b6c19
DillTheBeast <dillonmaltese@gmail.com>
2023-10-25 10:17:10 -0400

    asd

--- Triangle.py
+++ Triangle.py
@@ -4,17 +4,29 @@ screen = turtle.Screen()
 t = turtle.Turtle()
 t.speed(100)
 
-# Move the turtle to the desired position
-x_position = 100
-y_position = 100
 t.hideturtle()
 t.penup()  # Lift the pen
-t.goto(x_position, y_position)  # Move to the desired position
+t.goto(300, 300)  # Move to the desired position
 t.pendown()  # Put the pen down
 
 # Draw a dot
-dot_size = 10  # Adjust the size of the dot as needed
+dot_size = 5  # Adjust the size of the dot as needed
 t.dot(dot_size)
+
+
+t.goto(-300, 300)  # Move to the desired position
+
+# Draw a dot
+dot_size = 5  # Adjust the size of the dot as needed
+t.dot(dot_size)
+
+t.goto(0, 0)
+
+# Draw a dot
+dot_size = 5  # Adjust the size of the dot as needed
+t.dot(dot_size)
+
+t.goto(300, 300)
 
 # Keep the window open until it's closed by the user
 turtle.done()
