5880e38f589e6a48dac441b9702616d74723f797
DillTheBeast <dillonmaltese@gmail.com>
2023-10-25 14:30:50 -0400

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,16 +1,24 @@
 from PIL import Image, ImageDraw
+import time
 
 # Create a blank white image
 img = Image.new('RGB', (800, 600), color='white')
 
 # Use the ImageDraw module to fill the image
 draw = ImageDraw.Draw(img)
-# for x in range(-200, 200):
-#     for y in range(-200, 200):
-#         draw.point((x + 400, y + 300), fill='red')  # Adjusting for center
 
-for x in range(10, 50):
-    for y in range(100, 120):
-        draw.point((x, y), fill='black')
-
+#Square
+for x in range(100, 500):
+    for y in range(100, 500):
+        draw.point((x,y), fill='black')
 img.show()
+time.sleep(3)
+for x in range(100, 500):
+    for y in range(100, 500):
+        draw.point((x,y), fill='white')
+        time.sleep(1)
+#Right Triangle
+# for x in range(100, 500):
+#     for y in range(100, x):
+#         draw.point((x, y), fill='black')
+
