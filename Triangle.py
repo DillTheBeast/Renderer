9ab4db942a5d3b3564b3eb6edc2c9fe7af8c5e5b
DillTheBeast <dillonmaltese@gmail.com>
2023-10-26 14:43:27 -0400

    Update Triangle.py

--- Triangle.py
+++ Triangle.py
@@ -1,24 +1,51 @@
-from PIL import Image, ImageDraw
-import time
+import cv2
+import numpy as np
 
-# Create a blank white image
-img = Image.new('RGB', (800, 600), color='white')
+# Window dimensions
+WIDTH, HEIGHT = 800, 600
 
-# Use the ImageDraw module to fill the image
-draw = ImageDraw.Draw(img)
+# Create a white image (3 channels for RGB with all values 255 for white)
+img = np.ones((HEIGHT, WIDTH, 3), dtype=np.uint8) * 255
 
-#Square
-for x in range(100, 500):
-    for y in range(100, 500):
-        draw.point((x,y), fill='black')
-img.show()
-time.sleep(3)
-for x in range(100, 500):
-    for y in range(100, 500):
-        draw.point((x,y), fill='white')
-        time.sleep(1)
-#Right Triangle
-# for x in range(100, 500):
-#     for y in range(100, x):
-#         draw.point((x, y), fill='black')
+#Makes square
+# Set those pixel positions to black
+#for x in range(10, 500):
+#    for y in range(10, 500):
+#        img[y, x] = [0, 0, 0]  # [B, G, R] because OpenCV uses BGR format
 
+#Makes Cube
+for y in range(50, 500):
+    for x in range(10, 500):
+        img[y, x] = [0, 0, 0]
+
+x = 11
+count = 11
+for i in range(20):
+    for y in range(49, 1, -1):
+        img[y, x] = [0,0,0]
+        x += 1
+    count += 1
+    x = count
+
+x = 11
+count = 11
+for i in range(20):
+    for y in range(49, 1, -1):
+        img[y, x] = [0,0,0]
+        x += 1
+    count += 1
+    x = count
+
+
+
+# Keep the window open until a key press
+cv2.imshow('Window', img)
+cv2.waitKey(1)
+# Removes Square
+# for x in range(499, 9, -1):
+#     for y in range(499, 9, -1):
+#         img[y, x] = [255, 255, 255]  # [B, G, R] because OpenCV uses BGR format
+#     cv2.imshow('Window', img)
+#     cv2.waitKey(1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
