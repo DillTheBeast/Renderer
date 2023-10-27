105fbba356aa980956cd0e5cc082ce8a7e131600
DillTheBeast <dillonmaltese@gmail.com>
2023-10-27 10:04:43 -0400

    Update PyGameCube.py

--- PyGameCube.py
+++ PyGameCube.py
@@ -1,6 +1,6 @@
 import pygame
 import numpy as np
-import math
+from math import *
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -9,6 +9,10 @@ BLACK = (0, 0, 0)
 WIDTH, HEIGHT = 800, 600
 pygame.display.set_caption("3d Cube")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+scale = 100
+circle_pos = [WIDTH/2, HEIGHT/2]
+angle = 0
 
 points = []
 
@@ -25,8 +29,11 @@ projection_matrix = np.matrix([
     [1, 0, 0],
     [0, 1, 0]
 ])
+clock = pygame.time.Clock()
 
 while True:
+
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -37,12 +44,21 @@ while True:
                 exit()
 
     #update stuff
+    rotationZ = np.matrix([
+        [cos(angle), -sin(angle), 0],
+        [sin(angle), cos(angle), 0],
+        [0, 0, 1]
+    ])
+
+    angle += 0.01
 
     screen.fill(WHITE)
     #drawing stuff
     for point in points:
-        projected2D = np.dot(projection_matrix, point.reshape((3, 1)))
-        print(projected2D)
-        #pygame.draw.circle(screen, BLACK, (x, y), 5)
+        rotated2D = np.dot(rotationZ, point.reshape((3, 1)))
+        projected2D = np.dot(projection_matrix, rotated2D)
+        x = int(projected2D[0, 0] * scale) + circle_pos[0]
+        y = int(projected2D[1, 0] * scale) + circle_pos[1 ]
+        pygame.draw.circle(screen, BLACK, (x, y), 5)
 
     pygame.display.update()
