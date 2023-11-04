15440ca1d18a55e79244c388d315d44af2e2225e
DillTheBeast <dillonmaltese@gmail.com>
2023-11-04 14:30:23 -0400

    Update PyGameSelection.py

--- PyGameSelection.py
+++ PyGameSelection.py
@@ -110,7 +110,7 @@ def movement(points, projectedPoints, i, cube, triangle, pyramid, hexPrism):
         i += 1
     #Connecting the points
     if cube:
-        Cube.connectCubePoints(screen, cube.cubeProjectedPoints)
+        Cube.connectCubePoints(screen, Cube.cubeProjectedPoints)
     elif triangle:
         Triangle.connectTrianglePoints(connectPoints, projectedPoints)
     elif pyramid:
