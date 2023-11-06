b566ef1f6b679edfc58eb25fcae6e739b63be051
DillTheBeast <dillonmaltese@gmail.com>
2023-11-06 08:53:55 -0500

    asd

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -5,39 +5,38 @@
     // Updates
     // Render
 // 3, Clean up
+
+//Compile with this: g++ -o main main.cpp -I /opt/homebrew/opt/sdl2/include -L /opt/homebrew/opt/sdl2/lib -lSDL2
+
 #include <SDL2/SDL.h>
+#include <iostream>
 
-int main(int argc, char *argv[]) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        SDL_Log("SDL_Init Error: %s", SDL_GetError());
-        return 1;
+//Global Variables
+int gScreenHeight = 640;
+int gScreenWidth = 480;
+SDL_Window* gGraphicsAppWindow = nullptr;
+SDL_GLContext* gOpenGLContext = nullptr;
+
+void InitializeProgram() {
+    if(SDL_Init(SDL_INIT_VIDEO) < 0) {
+        std::cout << "SDL2 could not initialize video subsystem" << std::endl;
+        exit(1);
     }
+}
 
-    SDL_Window* window = SDL_CreateWindow("SDL Window", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 600, SDL_WINDOW_SHOWN);
+void MainLoop() {
 
-    if (window == nullptr) {
-        SDL_Log("SDL_CreateWindow Error: %s", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
+}
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    if (renderer == nullptr) {
-        SDL_Log("SDL_CreateRenderer Error: %s", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-
-    SDL_Delay(3000);  // Wait for 3 seconds
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+void CleanUp() {
     SDL_Quit();
+}
+
+int main() {
+
+    InitializeProgram();
+    MainLoop();
+    CleanUp();
+
     return 0;
 }
