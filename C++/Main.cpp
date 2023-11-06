4053e4768a043fa4fee7eb440d90129f8876da45
DillTheBeast <dillonmaltese@gmail.com>
2023-11-05 22:52:45 -0500

    Update Main.cpp

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -8,4 +8,15 @@
 
 #include <SDL2/SDL.h>
 
+int main(int argc, char *argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("SDL_Init Error: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Quit();
+    return 0;
+}
+
+
 
