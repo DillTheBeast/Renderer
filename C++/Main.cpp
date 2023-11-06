b8d87bfcc2c18ae794e7fde5b6e1d67bbc317741
DillTheBeast <dillonmaltese@gmail.com>
2023-11-06 13:42:40 -0500

    ad

--- C++/Main.cpp
+++ C++/Main.cpp
@@ -15,18 +15,29 @@
 int gScreenHeight = 640;
 int gScreenWidth = 480;
 SDL_Window* gGraphicsAppWindow = nullptr;
-SDL_GLContext* gOpenGLContext = nullptr; 
+SDL_GLContext gOpenGLContext; // Changed this line
+
+bool gQuit = false; //If true, we quit
 
 void InitializeProgram() {
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         std::cout << "SDL2 could not initialize video subsystem" << std::endl;
         exit(1);
     }
+
+    //Using OpenGL version 4.1
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+
     //Making the window
     //SDL_Window * SDL_CreateWindow("Renderer", 0, 0, gScreenWidth, gScreenHeight, SDL_WINDOW_OPENGL);
     gGraphicsAppWindow = SDL_CreateWindow("Renderer", 0, 0, gScreenWidth, gScreenHeight, SDL_WINDOW_OPENGL);
     if(gGraphicsAppWindow == nullptr) {
-        str::cout << "SDL Window was not able to be created" << std::endl;
+        std::cout << "SDL Window was not able to be created" << std::endl;
         exit(1);
     }
 
@@ -38,11 +49,42 @@ void InitializeProgram() {
     }
 }
 
+void Input() {
+    SDL_Event e;
+
+    while(SDL_PollEvent(&e) != 0) {
+        if(e.type == SDL_QUIT) {
+            std::cout << "See ya" << std::endl;
+            gQuit = true;
+        }
+    }
+}
+
+void PreDraw() {
+     
+}
+
+void Draw() {
+
+}
+
 void MainLoop() {
+
+    while(!gQuit) {
+        Input();
+
+        PreDraw();
+
+        Draw();
+
+        //Update the screen
+        SDL_GL_SwapWindow(gGraphicsAppWindow);
+    }
 
 }
 
 void CleanUp() {
+    SDL_DestroyWindow(gGraphicsAppWindow); // Also delete the window
     SDL_Quit();
 }
 
