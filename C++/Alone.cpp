e5cb4488a7c092fecbd1fce14c31d4ac186ed619
DillTheBeast <dillonmaltese@gmail.com>
2023-11-10 11:53:25 -0500

    Update Alone.cpp

--- C++/Alone.cpp
+++ C++/Alone.cpp
@@ -195,11 +195,13 @@ const char* fragmentShaderSource = R"(
 )";
 
 int main() {
+    //Making sure video can be loaded
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
         return -1;
     }
 
+    //Making window and making sure it can make window
     SDL_Window* window = SDL_CreateWindow("Spinning Cube", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
     if (!window) {
         std::cerr << "Failed to create SDL window: " << SDL_GetError() << std::endl;
@@ -207,6 +209,7 @@ int main() {
         return -1;
     }
 
+    //Setting up OpenGL context
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
@@ -219,6 +222,7 @@ int main() {
         return -1;
     }
 
+    //Initalizing GLAD
     if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
         std::cerr << "Failed to initialize GLAD" << std::endl;
         SDL_DestroyWindow(window);
