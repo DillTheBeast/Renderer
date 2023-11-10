d51a20c0eb0933dc3f620f34138b0584087a26f1
DillTheBeast <dillonmaltese@gmail.com>
2023-11-10 15:11:06 -0500

    ad

--- C++/Alone.cpp
+++ C++/Alone.cpp
@@ -1,170 +1,4 @@
-// #include <SDL2/SDL.h>
-// #include "glad/glad.h"
-// #include <glm/glm.hpp>
-// #include <glm/gtc/matrix_transform.hpp>
-// #include <glm/gtc/type_ptr.hpp>
-// #include <iostream>
-
-// const char* vertexShaderSource = R"(
-//     #version 330 core
-//     layout (location = 0) in vec3 aPos;
-//     uniform mat4 model;
-//     uniform mat4 view;
-//     uniform mat4 projection;
-//     void main()
-//     {
-//         gl_Position = projection * view * model * vec4(aPos, 1.0f);
-//     }
-// )";
-
-// const char* fragmentShaderSource = R"(
-//     #version 330 core
-//     out vec4 FragColor;
-//     void main()
-//     {
-//         FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
-//     }
-// )";
-
-// int main() {
-//     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-//         std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
-//         return -1;
-//     }
-
-//     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-//     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-//     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-
-//     SDL_Window* window = SDL_CreateWindow("Spinning Cube", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
-//     if (!window) {
-//         std::cerr << "Failed to create SDL window: " << SDL_GetError() << std::endl;
-//         SDL_Quit();
-//         return -1;
-//     }
-
-//     SDL_GLContext context = SDL_GL_CreateContext(window);
-//     if (!context) {
-//         std::cerr << "Failed to create OpenGL context: " << SDL_GetError() << std::endl;
-//         SDL_DestroyWindow(window);
-//         SDL_Quit();
-//         return -1;
-//     }
-
-//     if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
-//         std::cerr << "Failed to initialize GLAD" << std::endl;
-//         SDL_DestroyWindow(window);
-//         SDL_Quit();
-//         return -1;
-//     }
-
-//     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-//     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-//     glCompileShader(vertexShader);
-
-//     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-//     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-//     glCompileShader(fragmentShader);
-
-//     unsigned int shaderProgram = glCreateProgram();
-//     glAttachShader(shaderProgram, vertexShader);
-//     glAttachShader(shaderProgram, fragmentShader);
-//     glLinkProgram(shaderProgram);
-
-//     float vertices[] = {
-//         -0.5f, -0.5f, -0.5f,
-//          0.5f, -0.5f, -0.5f,
-//          0.5f,  0.5f, -0.5f,
-//         -0.5f,  0.5f, -0.5f,
-//         -0.5f, -0.5f,  0.5f,
-//          0.5f, -0.5f,  0.5f,
-//          0.5f,  0.5f,  0.5f,
-//         -0.5f,  0.5f,  0.5f
-//     };
-
-//     unsigned int indices[] = {
-//         0, 1, 2,
-//         2, 3, 0,
-//         4, 5, 6,
-//         6, 7, 4,
-//         0, 3, 7,
-//         7, 4, 0,
-//         1, 2, 6,
-//         6, 5, 1,
-//         0, 1, 5,
-//         5, 4, 0,
-//         2, 3, 7,
-//         7, 6, 2
-//     };
-
-//     unsigned int VAO, VBO, EBO;
-//     glGenVertexArrays(1, &VAO);
-//     glGenBuffers(1, &VBO);
-//     glGenBuffers(1, &EBO);
-
-//     glBindVertexArray(VAO);
-//     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-//     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-//     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-//     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-//     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-//     glEnableVertexAttribArray(0);
-
-//     glBindBuffer(GL_ARRAY_BUFFER, 0);
-//     glBindVertexArray(0);
-
-//     glEnable(GL_DEPTH_TEST);
-
-//     glm::mat4 model = glm::mat4(1.0f);
-//     glm::mat4 view = glm::lookAt(glm::vec3(3.0f, 3.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-//     glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-
-//     glUseProgram(shaderProgram);
-
-//     unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
-//     unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
-//     unsigned int projLoc = glGetUniformLocation(shaderProgram, "projection");
-
-//     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-//     glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
-
-//     while (true) {
-//         SDL_Event event;
-//         while (SDL_PollEvent(&event)) {
-//             if (event.type == SDL_QUIT) {
-//                 SDL_DestroyWindow(window);
-//                 SDL_Quit();
-//                 return 0;
-//             }
-//         }
-
-//         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-//         model = glm::rotate(model, glm::radians(0.01f), glm::vec3(0.5f, 1.0f, 0.0f));
-//         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-//         glBindVertexArray(VAO);
-//         glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
-
-//         SDL_GL_SwapWindow(window);
-//     }
-
-//     glDeleteVertexArrays(1, &VAO);
-//     glDeleteBuffers(1, &VBO);
-//     glDeleteBuffers(1, &EBO);
-
-//     glDeleteProgram(shaderProgram);
-//     glDeleteShader(fragmentShader);
-//     glDeleteShader(vertexShader);
-
-//     SDL_GL_DeleteContext(context);
-//     SDL_DestroyWindow(window);
-//     SDL_Quit();
-
-//     return 0;
-// }
+//COMPILE WITH: g++ -std=c++11 -o alone alone.cpp ./src/glad.c -I ./include/ -I /opt/homebrew/Cellar/glm/0.9.9.8/include/ -I /opt/homebrew/opt/sdl2/include -L /opt/homebrew/opt/sdl2/lib -lSDL2
 
 #include <SDL2/SDL.h>
 #include "glad/glad.h"
@@ -173,6 +7,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// Vertex shader source code
 const char* vertexShaderSource = R"(
     #version 330 core
     layout (location = 0) in vec3 aPos;
@@ -185,23 +20,33 @@ const char* vertexShaderSource = R"(
     }
 )";
 
+// Fragment shader source code
 const char* fragmentShaderSource = R"(
     #version 330 core
     out vec4 FragColor;
     void main()
     {
-        FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+        FragColor = vec4(1.0f, 0.0f, 0.0f, 1.0f);
     }
 )";
 
+//Global variables
+float rotationSpeed = 0.02f;
+float blueColor[] = {
+    0.0f, 
+    0.0f, 
+    1.0f, 
+    1.0f
+};
+
 int main() {
-    //Making sure video can be loaded
+        // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         std::cerr << "Failed to initialize SDL: " << SDL_GetError() << std::endl;
         return -1;
     }
 
-    //Making window and making sure it can make window
+    // Create an SDL window
     SDL_Window* window = SDL_CreateWindow("Spinning Cube", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
     if (!window) {
         std::cerr << "Failed to create SDL window: " << SDL_GetError() << std::endl;
@@ -209,7 +54,7 @@ int main() {
         return -1;
     }
 
-    //Setting up OpenGL context
+    // Create an OpenGL context
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
@@ -222,7 +67,7 @@ int main() {
         return -1;
     }
 
-    //Initalizing GLAD
+    // Load OpenGL functions with GLAD
     if (!gladLoadGLLoader(SDL_GL_GetProcAddress)) {
         std::cerr << "Failed to initialize GLAD" << std::endl;
         SDL_DestroyWindow(window);
@@ -230,6 +75,7 @@ int main() {
         return -1;
     }
 
+    // Vertex and fragment shader creation and linking
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
@@ -243,6 +89,7 @@ int main() {
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
 
+    // Vertex data for the cube
     float vertices[] = {
         -0.5f, -0.5f, -0.5f,
          0.5f, -0.5f, -0.5f,
@@ -254,6 +101,7 @@ int main() {
         -0.5f,  0.5f,  0.5f
     };
 
+    // Indices for drawing the cube
     unsigned int indices[] = {
         0, 1, 2,
         2, 3, 0,
@@ -269,41 +117,52 @@ int main() {
         7, 6, 2
     };
 
+    // Vertex Array Object (VAO), Vertex Buffer Object (VBO), and Element Buffer Object (EBO) creation
     unsigned int VAO, VBO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
 
+    // Bind the VAO
     glBindVertexArray(VAO);
 
+    // Bind and fill the VBO with vertex data
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+    // Bind and fill the EBO with indices
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
+    // Set vertex attribute pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+    // Unbind VBO and VAO
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-
     glBindVertexArray(0);
 
+    // Enable depth testing
     glEnable(GL_DEPTH_TEST);
 
+    // Model, view, and projection matrices
     glm::mat4 model = glm::mat4(1.0f);
     glm::mat4 view = glm::lookAt(glm::vec3(3.0f, 3.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
+    // Use the shader program
     glUseProgram(shaderProgram);
 
+    // Get uniform locations
     unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
     unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
     unsigned int projLoc = glGetUniformLocation(shaderProgram, "projection");
 
+    // Set the view and projection matrices
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
+    // Main loop
     bool quit = false;
     while (!quit) {
         SDL_Event event;
@@ -313,11 +172,13 @@ int main() {
             }
         }
 
+        glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
         // Clear the color and depth buffers
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // Set up the model matrix and rotate the cube
-        model = glm::rotate(model, glm::radians(0.02f), glm::vec3(0.5f, 1.0f, 0.0f));
+        model = glm::rotate(model, glm::radians(rotationSpeed), glm::vec3(0.5f, 1.0f, 0.0f));
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
         // Draw the cube
@@ -328,6 +189,7 @@ int main() {
         SDL_GL_SwapWindow(window);
     }
 
+    // Clean up
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
