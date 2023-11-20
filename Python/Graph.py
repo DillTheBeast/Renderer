911f035339467d5580e2e2259d16a1474be23f58
DillTheBeast <68816809+DillTheBeast@users.noreply.github.com>
2023-11-20 13:16:43 -0500

    Update Graph.py

--- Python/Graph.py
+++ Python/Graph.py
@@ -3,18 +3,25 @@ import numpy as np
 
 pygame.init()
 
+#Defining pygame needed variables
 WIDTH, HEIGHT = 1400, 800
 pygame.display.set_caption("Graph")
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 font = pygame.font.Font(None, 36)
 
+#Defining color codes
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (50, 120, 255)
 IDK = (255, 120, 50)
+
+#Array used to store all of the poins
+#Going to be a 2d array b/c 1 for x and 1 for y axis
 points = []
 
+#Going to make lines for the equations
 def connectPoints(i, j, points, COLOR):
+    #Defining 2 points for each line
     x1, y1 = points[i]
     x2, y2 = points[j]
 
@@ -34,10 +41,14 @@ def connectPoints(i, j, points, COLOR):
     # Draw the extended line
     pygame.draw.line(screen, COLOR, (extended_x1, extended_y1), (extended_x2, extended_y2), 3)
 
+#Function to make the grid lines + origin point
 def drawGrid():
+    #Drawing origin point
     pygame.draw.circle(screen, WHITE, (WIDTH // 2, HEIGHT // 2), 5)
     x = 0  
-    y = 0 
+    y = 0
+    #Making x + y grid lines
+    #Each line is 50 pixels apart on x + y axis
     for i in range((1400 // 50) + 1):  
         x += 50  
         pygame.draw.line(screen, WHITE, (x, 0), (x, HEIGHT))
@@ -64,7 +75,7 @@ def wrap_text(text, font, max_width):
 
 def inputText(display_text, initial_value=""):
     input_window = pygame.display.set_mode((1400, 800))
-    pygame.display.set_caption("Text Input")
+    pygame.display.set_caption("Graph Calculator")
     user_text = initial_value
 
     while True:
@@ -93,41 +104,53 @@ def inputText(display_text, initial_value=""):
         pygame.display.flip()
 
 def findPoints():
-    neg_choice = int(inputText("If m is a negative number press 1. Otherwise click any other number: \n"))
-    mNum = int(inputText("What is m's numerator: \n"))
-    mDen = int(inputText("What is m's denominator: \n"))
-    bAns = int(inputText("What is b: \n"))
     bP = 0
     mDenP1 = 0
     mDenP2 = 0
     mDenP3 = 0
     mDenP4 = 0
-    for i in range(bAns):
-        bP += 50
-    mNumP1 = HEIGHT/2 - bP
-    mNumP2 = HEIGHT/2 - bP
-    mNumP3 = HEIGHT/2 - bP
-    mNumP4 = HEIGHT/2 - bP
-    points.append((WIDTH/2, HEIGHT/2 - bP))
-    for i in range(mNum):
-        mNumP1 -= 50
-        mNumP2 += 50
-        mNumP3 -= 50  # Corrected
-        mNumP4 += 50  # Corrected
-    for i in range(mDen):
-        mDenP1 += 50
-        mDenP2 -= 50
-        mDenP3 += 50  # Corrected
-        mDenP4 -= 50  # Corrected
-    points.append((WIDTH/2 + mDenP1, mNumP1))
-    points.append((WIDTH/2 + mDenP2, mNumP2))
-    points.append((WIDTH/2 + mDenP3, mNumP3))
-    points.append((WIDTH/2 + mDenP4, mNumP4))
+    mNumP1 = HEIGHT / 2 - bP
+    mNumP2 = HEIGHT / 2 - bP
+    mNumP3 = HEIGHT / 2 - bP
+    mNumP4 = HEIGHT / 2 - bP
+    #Stuff needs to be a little different with neg slope
+    negChoice = int(inputText("If m is a negative number press 1. Otherwise click any other number: \n"))
+    mNum = int(inputText("What is m's numerator: \n"))
+    mDen = int(inputText("What is m's denominator: \n"))
+    bAns = int(inputText("What is b: \n"))
+
+    if negChoice == 1:
+        print("Test")
+
+    if bAns > 0:
+        #Making y intercept
+        for i in range(bAns):
+            bP += 50
+        #Making the numerator + denominator points
+        for i in range(mNum):
+            mNumP1 -= 50
+            mNumP2 += 50
+            mNumP3 -= 50
+            mNumP4 += 50
+        for i in range(mDen):
+            mDenP1 += 50
+            mDenP2 -= 50
+            mDenP3 += 50
+            mDenP4 -= 50
+        #Adding all of the points created
+        points.append((WIDTH / 2, HEIGHT / 2 - bP))
+        points.append((WIDTH/2 + mDenP1, mNumP1))
+        points.append((WIDTH/2 + mDenP2, mNumP2))
+        points.append((WIDTH/2 + mDenP3, mNumP3))
+        points.append((WIDTH/2 + mDenP4, mNumP4))
 
 
 running = True
+#Making the amount of line equations there will be
 for i in range(2):
     findPoints()
+
+#Main loop
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -136,6 +159,7 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
+    #Setting everything up
     screen.fill(BLACK)
     drawGrid()
     for i in range(9):
