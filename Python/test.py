ba614d8ce98f2f90e2eaeec338eb13a5b9020ae2
DillTheBeast <dillonmaltese@gmail.com>
2023-12-15 21:43:12 -0500

    ghghj

--- Python/test.py
+++ Python/test.py
@@ -1,239 +1,62 @@
-import pygame
+import pyglet
+from pyglet.gl import *
 import numpy as np
-from math import *
-import sys
-from Cube import Cube
-from Triangle import Triangle
-from Pyramid import Pyramid
-from Sphere import Sphere
-from HexPrism import HexPrism
-from ColorsAndText import ColorsAndText
-from Physics import Physics
+import ctypes
 
-pygame.init()
+model_path = "bowl.obj"
 
-paused = False
-forward = False
-triangle = False
-pyramid = False
-cube = True
-hexPrism = False
-sphere = False
-moveUp = False
-moveDown = False
-moveLeft = False
-moveRight = False
+# Load OBJ file manually
+vertices = []
+faces = []
 
-WIDTH, HEIGHT = 1440, 845
-pygame.display.set_caption("3d Renderer")
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+with open(model_path, 'r', encoding='latin-1') as obj_file:
+    for line in obj_file:
+        if line.startswith('v '):
+            vertices.append(list(map(float, line.split()[1:])))
+        elif line.startswith('f '):
+            faces.append([int(vertex.split('/')[0]) - 1 for vertex in line.split()[1:]])
 
-scale = 96
-circle_pos = [WIDTH / 2, HEIGHT / 2]
-angle = 0
-xAngle = 0
-yAngle = 0
-angleAddition = 0.01
-wall_thickness = 10
-gravity = 0.5
-bounceStop = 0.3
-viewing_angle = 0
+# Flatten the faces list
+flat_faces = [vertex for face in faces for vertex in face]
 
-# For Cube
-Cube = Cube()
-Cube.appendCubePoints()
+# Convert to NumPy arrays
+vertices_np = np.array(vertices, dtype=np.float32)
+faces_np = np.array(flat_faces, dtype=np.uint32)
 
-# For Triangle
-Triangle = Triangle()
-Triangle.appendTrianglePoints()
+# Set up Pyglet window
+window = pyglet.window.Window()
 
-# For Pyramid
-Pyramid = Pyramid()
-Pyramid.appendPyramidPoints()
+# Enable depth testing
+glEnable(GL_DEPTH_TEST)
 
-#For Hexagon Prism
-HexPrism = HexPrism()
-HexPrism.appendHexPrismPoints()
-#For Sp65555555here
-Sphere = Sphere()
-Sphere.appendSpherePoints()
+# Create VBO (Vertex Buffer Object)
+vbo = GLuint()
+glGenBuffers(1, ctypes.byref(vbo))
+glBindBuffer(GL_ARRAY_BUFFER, vbo)
+glBufferData(GL_ARRAY_BUFFER, vertices_np.nbytes, vertices_np.ctypes.data_as(ctypes.POINTER(GLfloat)), GL_STATIC_DRAW)
 
-#For the colors and the texts displayed
-ColorsAndText = ColorsAndText()
-ColorsAndText.addColors()
+# Create IBO (Index Buffer Object)
+ibo = GLuint()
+glGenBuffers(1, ctypes.byref(ibo))
+glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo)
+glBufferData(GL_ELEMENT_ARRAY_BUFFER, faces_np.nbytes, faces_np.ctypes.data_as(ctypes.POINTER(GLuint)), GL_STATIC_DRAW)
 
-#For the physics
-# physics = Physics()
+# Create VAO (Vertex Array Object)
+vao = GLuint()
+glGenVertexArrays(1, ctypes.byref(vao))
+glBindVertexArray(vao)
+glEnableVertexAttribArray(0)
+glBindBuffer(GL_ARRAY_BUFFER, vbo)
+glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, ctypes.c_void_p(0))
+glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo)
+glBindVertexArray(0)
 
-projection_matrix = np.matrix([
-    [1, 0, 0],
-    [0, 1, 0]
-])
+@window.event
+def on_draw():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-def connectPoints(i, j, points):
-    pygame.draw.line(screen, ColorsAndText.shapeColor, (points[i][0], points[i][1]), (points[j][0], points[j][1]))
+    glBindVertexArray(vao)
+    glDrawElements(GL_TRIANGLES, len(flat_faces), GL_UNSIGNED_INT, ctypes.c_void_p(0))
+    glBindVertexArray(0)
 
-
-def checkShapes(change, object1, object2, object3, object4):
-    if object1:
-        object1 = not object1
-    elif object2: 
-        object2 = not object2
-    elif object3:
-        object3 = not object3
-    elif object4:
-        object4 = not object4
-
-    change = not change
-
-    return change, object1, object2, object3, object4
-
-
-def movement(shape_instance):
-    points = shape_instance.cubePoints
-    projectedPoints = shape_instance.cubeProjectedPoints
-    i = 0
-
-    # Making dots at updated/rotated points
-    for point in points:
-        rotated2D = np.dot(rotationZ, point.reshape((3, 1)))
-        rotated2D = np.dot(rotationX, rotated2D)
-        rotated2D = np.dot(rotationY, rotated2D)
-
-        projected2D = np.dot(projection_matrix, rotated2D)
-
-        x = int(projected2D[0, 0] * scale) + circle_pos[0]
-        y = int(projected2D[1, 0] * scale) + circle_pos[1]
-
-        if i >= len(projectedPoints):
-            projectedPoints.append([x, y])
-        else:
-            projectedPoints[i] = [x, y]
-        pygame.draw.circle(screen, shape_instance.color, (x, y), 5)
-        i += 1
-
-    # Connecting the points
-    shape_instance.connectCubePoints(screen, projectedPoints, connectPoints)
-
-
-clock = pygame.time.Clock()
-
-while True:
-
-    clock.tick(60)
-    for event in pygame.event.get():
-        #Checking what key is pressed and then executing specific action
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                exit()
-            if event.key == pygame.K_SPACE: paused = not paused
-            if event.key == pygame.K_r: forward = not forward
-            if event.key == pygame.K_UP: scale += 96
-            if event.key == pygame.K_DOWN: scale -= 96
-            if event.key == pygame.K_c: cube, pyramid, triangle, hexPrism, sphere = checkShapes(cube, pyramid, triangle, hexPrism, sphere)
-            if event.key == pygame.K_p: pyramid, cube, triangle, hexPrism, sphere = checkShapes(pyramid, cube, triangle, hexPrism, sphere)
-            if event.key == pygame.K_t: triangle, pyramid, cube, hexPrism, sphere = checkShapes(triangle, pyramid, cube, hexPrism, sphere)
-            if event.key == pygame.K_h: hexPrism, pyramid, cube, triangle, sphere = checkShapes(hexPrism, pyramid, cube, triangle, sphere)
-            if event.key == pygame.K_l: sphere, pyramid, cube, triangle, hexPrism = checkShapes(sphere, pyramid, cube, triangle, hexPrism)
-            if event.key == pygame.K_f: angleAddition += 0.01
-            if event.key == pygame.K_b: angleAddition -= 0.01
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:  # Check if left mouse button is clicked
-                ColorsAndText.x, ColorsAndText.y = pygame.mouse.get_pos()
-                ColorsAndText.changeColors()
-                if ColorsAndText.x > 100 and (ColorsAndText.x < 1000 and ColorsAndText.y > 35):
-                    circle_pos = [ColorsAndText.x, ColorsAndText.y]
-                    
-    keys = pygame.key.get_pressed()
-
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_w]:
-        yAngle += angleAddition
-    if keys[pygame.K_s]:
-        yAngle -= angleAddition
-    if keys[pygame.K_d]:
-        xAngle += angleAddition
-    if keys[pygame.K_a]:
-        xAngle -= angleAddition
-
-    # update rotation matrices based on angles
-    rotationZ = np.matrix([
-        [cos(viewing_angle), -sin(viewing_angle), 0],
-        [sin(viewing_angle), cos(viewing_angle), 0],
-        [0, 0, 1]
-    ])
-
-    rotationY = np.matrix([
-        [cos(xAngle), 0, sin(xAngle)],
-        [0, 1, 0],
-        [-sin(xAngle), 0, cos(xAngle)]
-    ])
-
-    rotationX = np.matrix([
-        [1, 0, 0],
-        [0, cos(yAngle), -sin(yAngle)],
-        [0, sin(yAngle), cos(yAngle)]
-    ])
-    
-    if not paused:
-        
-        if ColorsAndText.falling:
-            #Need to move object down
-            circle_pos[1] += 5
-        # update shape rotations
-        # rotationZ = np.matrix([
-        #     [cos(viewing_angle), -sin(viewing_angle), 0],
-        #     [sin(viewing_angle), cos(viewing_angle), 0],
-        #     [0, 0, 1]
-        # ])
-        # rotationY = np.matrix([
-        #     [cos(xAngle), 0, sin(xAngle)],
-        #     [0, 1, 0],
-        #     [-sin(xAngle), 0, cos(xAngle)]
-        # ])
-        # rotationX = np.matrix([
-        #     [1, 0, 0],
-        #     [0, cos(yAngle), -sin(yAngle)],
-        #     [0, sin(yAngle), cos(yAngle)]
-        # ])
-
-        #if forward:
-        #    angle += angleAddition
-        #else:
-        #    angle -= angleAddition
-
-        screen.fill(ColorsAndText.backgroundColor)
-        #Finding out which shape I want to display and displaying it
-        if cube:
-            movement(Cube)
-        elif triangle:
-            movement(Triangle)
-        elif pyramid:
-            movement(Pyramid)
-        elif hexPrism:
-            movement(HexPrism)
-        elif sphere:
-            movement(Sphere)
-
-    place = 20
-    for i in range(2):
-        if i == 0:
-            header = ColorsAndText.header1
-        else:
-            header = ColorsAndText.header2
-            
-        screen.blit(header, (20, place))
-        place += 35
-        
-        for j in range(1, len(ColorsAndText.texts)):
-            screen.blit(ColorsAndText.texts[j], (20, place))
-            place += 35
-        place += 40
-    screen.blit(ColorsAndText.runButton, (WIDTH-55, 10))
-
-    pygame.display.update()
+pyglet.app.run()
--- Python/testing.py
+++ Python/testing.py
@@ -1,74 +1,119 @@
 import pygame
 from pygame.locals import *
 from OpenGL.GL import *
+from OpenGL.GLUT import *
 from OpenGL.GLU import *
 import glm
 import math
-import numpy
-import ctypes
+import numpy as np
 
-# Load 3D model (replace 'your_model.obj' with the path to your model file)
-model_path = 'sushi.obj'
+# Load 3D model (replace 'sushi.obj' with the path to your model file)
+model_path = 'bowl.obj'
 model_vertices = []
-model_faces = []
 
 # Scaling factor to make the model smaller
-scale_factor = 0.5  # You can adjust this value based on how much you want to scale the model
+scale_factor = 0.5
 
 with open(model_path, 'r', encoding='latin-1') as file:
     for line in file:
         if line.startswith('v '):
             vertices = line.split()[1:]
             model_vertices.append([float(v) for v in vertices])
-        elif line.startswith('f '):
-            faces = line.split()[1:]
-            model_faces.append([int(f.split('/')[0]) - 1 for f in faces])
 
 # Apply scaling to the entire model
-scaled_model_vertices = [[v[0] * scale_factor, v[1] * scale_factor, v[2] * scale_factor] for v in model_vertices]
+scaled_model_vertices = np.array(model_vertices) * scale_factor
 
-WIDTH, HEIGHT = 800, 600
-
+# Initialize Pygame
 pygame.init()
-pygame.display.set_mode((WIDTH, HEIGHT), DOUBLEBUF | OPENGL | pygame.OPENGL_COMPATBLT)
 
+# Set desired OpenGL attributes
+pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MAJOR_VERSION, 3)
+pygame.display.gl_set_attribute(pygame.GL_CONTEXT_MINOR_VERSION, 3)
+pygame.display.gl_set_attribute(pygame.GL_CONTEXT_PROFILE_MASK, pygame.GL_CONTEXT_PROFILE_CORE)
+
+# Try to create the screen with the OPENGL flag
+try:
+    screen = pygame.display.set_mode((800, 600), DOUBLEBUF | OPENGL)
+except pygame.error as e:
+    print(f"Error creating OpenGL context: {e}")
+    pygame.quit()
+    quit()
 
 # Constants
 FPS = 60
 
-# Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-
-# Create the screen
-screen = pygame.display.set_mode((WIDTH, HEIGHT), DOUBLEBUF | OPENGL)
-pygame.display.set_caption("Simple 3D Model Viewer")
-clock = pygame.time.Clock()
-
-# Camera parameters
-camera_pos = glm.vec3(0, 0, 5)  # Initial camera position
-yaw = 0  # Yaw angle (rotation around the y-axis)
-pitch = 0  # Pitch angle (rotation around the x-axis)
-
-# OpenGL setup
+# Set up the OpenGL context
 glEnable(GL_DEPTH_TEST)
 glEnable(GL_CULL_FACE)
 
-# Perspective setup
-glMatrixMode(GL_PROJECTION)
-gluPerspective(45, (WIDTH / HEIGHT), 0.1, 100.0)
-glMatrixMode(GL_MODELVIEW)
-glTranslatef(0.0, 0.0, -5)
+# Vertex and fragment shader code
+vertex_shader = """
+#version 330 core
+layout(location = 0) in vec3 in_position;
+uniform mat4 projection_matrix;
+uniform mat4 modelview_matrix;
 
-# After loading the model, create VBO and VAO
-vbo = glGenBuffers(1)
-glBindBuffer(GL_ARRAY_BUFFER, vbo)
-glBufferData(GL_ARRAY_BUFFER, numpy.array(scaled_model_vertices, dtype='float32'), GL_STATIC_DRAW)
+void main()
+{
+    gl_Position = projection_matrix * modelview_matrix * vec4(in_position, 1.0);
+}
+"""
 
+fragment_shader = """
+#version 330 core
+out vec4 fragColor;
+
+void main()
+{
+    fragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+"""
+
+# Compile shaders
+shader_program = glCreateProgram()
+vertex_shader_obj = glCreateShader(GL_VERTEX_SHADER)
+fragment_shader_obj = glCreateShader(GL_FRAGMENT_SHADER)
+
+glShaderSource(vertex_shader_obj, vertex_shader)
+glCompileShader(vertex_shader_obj)
+glAttachShader(shader_program, vertex_shader_obj)
+
+glShaderSource(fragment_shader_obj, fragment_shader)
+glCompileShader(fragment_shader_obj)
+glAttachShader(shader_program, fragment_shader_obj)
+
+glLinkProgram(shader_program)
+glUseProgram(shader_program)
+
+# Create VBO and VAO
 vao = glGenVertexArrays(1)
 glBindVertexArray(vao)
-glEnableVertexAttribArray(0)
-glVertexAttribPointer(0, 3, GL_FLOAT, False, 0, ctypes.c_void_p(0))
+
+vbo = glGenBuffers(1)
+glBindBuffer(GL_ARRAY_BUFFER, vbo)
+glBufferData(GL_ARRAY_BUFFER, scaled_model_vertices, GL_STATIC_DRAW)
+
+# Get the attribute location, enable it, and set the pointer
+attribute_location = glGetAttribLocation(shader_program, "in_position")
+glEnableVertexAttribArray(attribute_location)
+glVertexAttribPointer(attribute_location, 3, GL_FLOAT, False, 0, None)
+
+# Unbind VAO
+glBindVertexArray(0)
+
+# Projection matrix setup
+projection_matrix_location = glGetUniformLocation(shader_program, "projection_matrix")
+projection_matrix = glm.perspective(glm.radians(45.0), 800 / 600, 0.1, 100.0)
+glUniformMatrix4fv(projection_matrix_location, 1, GL_FALSE, glm.value_ptr(projection_matrix))
+
+# Modelview matrix setup
+modelview_matrix_location = glGetUniformLocation(shader_program, "modelview_matrix")
+
+# Camera parameters for top-down view
+camera_pos = glm.vec3(0, 10, 0)
+yaw, pitch = 0, -math.pi / 2  # Look straight down
+
+clock = pygame.time.Clock()
 
 # Main game loop
 while True:
@@ -77,51 +122,25 @@ while True:
             pygame.quit()
             quit()
 
-    # Handle camera movement and rotation
+    # Handle camera movement
     keys = pygame.key.get_pressed()
     speed = 0.1
-    rotation_speed = 0.02
 
     if keys[pygame.K_w]:
-        camera_pos.z -= speed * glm.cos(yaw)
-        camera_pos.x += speed * glm.sin(yaw)
-        camera_pos.y += speed * glm.sin(pitch)
-
+        camera_pos.z -= speed
     if keys[pygame.K_s]:
-        camera_pos.z += speed * glm.cos(yaw)
-        camera_pos.x -= speed * glm.sin(yaw)
-        camera_pos.y -= speed * glm.sin(pitch)
-
-    if keys[pygame.K_a]:
-        yaw += rotation_speed
-
-    if keys[pygame.K_d]:
-        yaw -= rotation_speed
-
+        camera_pos.z += speed
     if keys[pygame.K_q]:
         camera_pos.y -= speed
-
     if keys[pygame.K_e]:
         camera_pos.y += speed
-
-    if keys[pygame.K_UP]:
-        pitch += rotation_speed
-
-    if keys[pygame.K_DOWN]:
-        pitch -= rotation_speed
-
-    pitch = max(-math.pi / 2, min(math.pi / 2, pitch))
 
     # Clear the screen
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    # Load the identity matrix to reset transformations
-    glLoadIdentity()
-
-    # Apply camera transformations
-    gluLookAt(camera_pos.x, camera_pos.y, camera_pos.z,
-              camera_pos.x + glm.sin(yaw), camera_pos.y + glm.sin(pitch), camera_pos.z - glm.cos(yaw),
-              0, 1, 0)
+    # Modelview matrix setup
+    modelview_matrix = glm.lookAt(camera_pos, glm.vec3(0, 0, 0), glm.vec3(0, 0, 1))
+    glUniformMatrix4fv(modelview_matrix_location, 1, GL_FALSE, glm.value_ptr(modelview_matrix))
 
     # Draw the 3D model
     glBindVertexArray(vao)
